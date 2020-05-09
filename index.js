4f6133a1fa25e11b63360fd6dd95ba523656ed3f
simpart <simpart>
2020-05-09 03:33:56 +0000

    supported v1.1

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
  * @brief visible switch click event for mofron
  * @author simpart
  */
-const mf = require('mofron');
 const Click = require('mofron-event-click');
 
-mf.event.VisiClick = class extends Click {
+module.exports = class extends Click {
     /**
      * initilize event
      *
@@ -14,23 +13,47 @@ mf.event.VisiClick = class extends Click {
      * @param p1 (string) visible mode
      * @param p2 (Component) target component
      */
-    constructor (po, p2) {
+    constructor (p1, p2) {
         try {
             super();
             this.name('VisiClick');
-            this.prmMap(['mode', 'tgtComp']);
-            this.prmOpt(po, p2);
-            this.handler(
-                (tgt, vs) => {
+            this.shortForm('mode', 'tgtComp');
+	    
+	    /* init config */
+            this.confmng().add("callback", { type: "function" });
+	    this.confmng().add("tgtComp",  { type: "Component" });
+            this.confmng().add("mode",     { type: "string", init: "switch" });
+
+	    /* set config */
+	    if (0 < arguments.length) {
+                this.config(p1,p2);
+	    }
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * add click event to target component.
+     *
+     * @param (component) event target component
+     * @type private
+     */
+    contents (tgt_dom) {
+        try {
+            super.contents(tgt_dom);
+            this.listener(
+                (tgt, ev, vs) => {
                     try {
                         if ('enable' === vs.mode()) {
-                            vs.tgtComp().visible(true, this.tgtParam());
+                            vs.tgtComp().visible(true, this.callback());
                         } else if ('disable' === vs.mode()) {
-                            vs.tgtComp().visible(false, this.tgtParam());
+                            vs.tgtComp().visible(false, this.callback());
                         } else if ('destroy' === vs.mode()) {
                             vs.tgtComp().destroy(this.tgtParam());
                         } else if ('switch' === vs.mode()) {
-                            vs.tgtComp().visible(!vs.tgtComp().visible(), this.tgtParam());
+                            vs.tgtComp().visible(!vs.tgtComp().visible(), this.callback());
                         }
                     } catch (e) {
                         console.error(e.stack);
@@ -39,7 +62,8 @@ mf.event.VisiClick = class extends Click {
                 },
                 this
             );
-        } catch (e) {
+            
+	} catch (e) {
             console.error(e.stack);
             throw e;
         }
@@ -51,27 +75,29 @@ mf.event.VisiClick = class extends Click {
      * @param p1 (Component) set visible target component
      * @param p1 (undefined) call as getter
      * @return (Component) visible target component
+     * @type parameter
      */
     tgtComp (prm) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_tgtcomp) ? this.component() : this.m_tgtcomp;
-            }
-            /* setter */
-            if (true !== mf.func.isInclude(prm, 'Component')) {
-                throw new Error('invalid parameter');
-            }
-            this.m_tgtcomp = prm;
+	    let ret = this.confmng("tgtComp", prm);
+	    return (null === ret) ? this.component() : ret;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    tgtParam (prm) {
+    /**
+     * callback function for switch
+     * 
+     * @param (function) callback function
+     * @return (function) callback function
+     * @type parameter
+     */
+    callback (prm) {
         try {
-            return (undefined === prm) ? this.m_tgtprm : this.m_tgtprm = prm;
+	    let ret = this.confmng("callback", prm);
+            return (null === ret) ? undefined : ret;
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -87,12 +113,11 @@ mf.event.VisiClick = class extends Click {
      */
     mode (prm) {
         try {
-            return this.member('mode', ['enable', 'disable', 'destroy', 'switch'], prm, 'switch');
+	    return this.confmng("mode", prm);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
 }
-module.exports = mofron.event.VisiClick;
 /* end of file */
